16c44f7fe14ef21d30185971554d5e1847abd710
HP <HP@DESKTOP-862H2AA>
2023-01-22 23:19:30 +0530

    my night repo is ready to share

--- src/mynighthrepo/nightrepo.java
+++ src/mynighthrepo/nightrepo.java
@@ -5,7 +5,7 @@ public class nightrepo {
 	public static void main(String[] args) {
 		System.out.println("night repo");
 	System.out.println(90+110);
-
+System.out.println("my night repo");
 	}
 
 }
